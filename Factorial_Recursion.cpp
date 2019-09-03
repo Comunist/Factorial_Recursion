2ad9c8c5af89fa956cf6c6fce2bdaa07c0b2b805
Ilya <iliya.yurkevic@mail.ru>
2019-09-03 20:33:55 +0300

    Добавьте файлы проекта.

---
+++ Factorial_Recursion.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fact(int N)
+{
+	if (N == 0) return 1;
+	return N * fact(N - 1);
+}
+
+int main()
+{
+	int N;
+	int F = 1;
+	setlocale(LC_ALL, "rus");
+	cout << "Факториал какого числа необходимо вычислить?\n";
+	cin >> N;
+	if (N < 0)
+	{
+		cerr << "Недопустимое значение. Введите число >= 0\n";
+		return 1;
+	}
+	cout << "Факториал " << N << " = " << fact(N) << endl;
+	return 0;
+}
